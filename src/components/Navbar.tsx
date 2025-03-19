817ac2240a31fba47f68ef246187a19d8a1cf64f
anamul101 <rjanamul01871@gmail.com>
2025-03-19 14:28:58 +0600

    nav button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,64 @@
 import Image from "next/image";
-import logosaas from "../assets/images/logosaas.png"
-import Menu from "../assets/icons/menu.svg"
+import logosaas from "../assets/images/logosaas.png";
+import Menu from "../assets/icons/menu.svg";
+import Button from "@/utility/button";
 
 export const Navbar = () => {
-  return(
-    <div className="bg-black">
-        <div className="px-4">
-      <div className="py-4 flex items-center justify-between">
-      <div className="relative">
-        <div className="absolute w-full top-2 bottom-0 bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] blur-md"></div>
-      <Image src={logosaas} alt="sass logo" className="h-12, w-12 relative"/>
+  return (
+    <div className="bg-[#020202]">
+      <div className="sm:px-10 px-4">
+        <div className="py-4 flex items-center justify-between">
+          <div className="relative">
+            <div className="absolute w-full top-2 bottom-0 bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] blur-md"></div>
+            <Image
+              src={logosaas}
+              alt="sass logo"
+              className="h-12, w-12 relative"
+            />
+          </div>
+          <div className="border border-white border-opacity-30 h-10 w-10 inline-flex items-center justify-center rounded-lg sm:hidden">
+            <Menu className="text-white" />
+          </div>
+          <nav className="gap-6 items-center hidden sm:flex text-sm ">
+            <a
+              href="#"
+              className="capitalize text-opacity-100 text-white transition-all duration-300 hover:text-opacity-75 text transtion"
+            >
+              services
+            </a>
+            <a
+              href="#"
+              className="capitalize text-opacity-100 text-white transition-all duration-300 hover:text-opacity-75 text transtion"
+            >
+              product
+            </a>
+            <a
+              href="#"
+              className="capitalize text-opacity-100 text-white transition-all duration-300 hover:text-opacity-75 text transtion"
+            >
+              About
+            </a>
+            <a
+              href="#"
+              className="capitalize text-opacity-100 text-white transition-all duration-300 hover:text-opacity-75 text transtion"
+            >
+              projects
+            </a>
+            <a
+              href="#"
+              className="capitalize text-opacity-100 text-white transition-all duration-300 hover:text-opacity-75 text transtion"
+            >
+              testimonial
+            </a>
+            <div className="flex items-center">
+              <Button />
+              <a className="font-normal text-xs md:text-sm leading-5 block select-none cursor-pointer transition-all duration-300 hover:text-opacity-75 text-white">
+                Log In
+              </a>
+            </div>
+          </nav>
+        </div>
       </div>
-      <div className="border border-white border-opacity-30 h-10 w-10 inline-flex items-center justify-center rounded-lg sm:hidden">
-      <Menu className="text-white"/>
-      </div>
-      <nav className="gap-6 items-center hidden sm:flex">
-        <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transtion">services</a>
-        <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transtion">product</a>
-        <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transtion">About</a>
-        <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transtion">projects</a>
-        <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transtion">testimonial</a>
-        <button className="bg-white py-2 px-4 rounded-md">get in touch</button>
-      </nav>
-      </div>
-    </div>
     </div>
   );
-  
 };
