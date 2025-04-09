c589df1917ab2851d2be3e2d767e6a33cf6b9c22
anamul101 <rjanamul01871@gmail.com>
2025-04-09 20:51:22 +0600

    add

---
+++ src/components/products/ProductsHeroSection.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const ProductsHeroSection = () => {
+  return (
+    <section className="ρd__all ρd__section ρ_3ZqLs">
+      <div className="ρd__all ρd__div ρx6Tz7 container">
+        <div className="ρd__all ρd__div ρqItuK">
+          <div className="ρd__all ρd__div ρ_4N8M">
+            <a className="ρd__all ρd__a ρt ρkqoAi" href="/data-engine">
+              Scale Data Engine
+            </a>
+          </div>
+        </div>
+        <div className="ρd__all ρd__div ρtc8OA">
+          <div className="ρd__all ρd__div ρdp9Ub">
+            <div className="flex flex-col items-center sm:items-start">
+              <div className="flex flex-col items-center justify-center w-full text-center sm:items-start lg:justify-start md:flex-row sm:text-left">
+                <div className="relative z-10 flex flex-col items-center w-full sm:items-start">
+                  <h1 className="font-aeonik text-4.5xl lg:text-5xl sm:pr-12 md:pr-40 lg:pr-0 w-full [text-wrap:balance]">
+                    Generative AI Data Engine
+                  </h1>
+                </div>
+              </div>
+              <h2 className="w-full text-lg text-center text-gray-300 mb-8 lg:text-xl mt-2 lg:leading-8 sm:pr-8 xl:pr-20 sm:text-left [text-wrap:balance]">
+                Powering the Next Frontier of AI.
+              </h2>
+              <div className="flex items-center">
+                <div>
+                  <button
+                    className="relative focus-visible:outline outline-[rgba(255,255,255,0.64)] rounded-xl outline-offset-[3px]"
+                    style={{ WebkitTapHighlightColor: 'transparent' }}
+                  >
+                    <div className="new-button_button--primary__EJ4AI font-medium justify-center flex flex-nowrap whitespace-nowrap transition-translate duration-300 cursor-pointer group items-center h-full group leading-[150%] new-button_button--purple__hhI2y text-sm px-[30px] py-[14px]">
+                      Book a Demo
+                      <span className="inline-block ml-1 font-normal duration-300 w-fit transition-translate group-hover:translate-x-1 font-unicode">
+                        →
+                      </span>
+                    </div>
+                    <span className="after:content-[''] after:inline-block absolute inset-0 after:inset-0 after:absolute select-none pointer-events-none after:rounded-[11px] rounded-[11px] shadow-[inset_0_0_0_1px_rgba(255,255,255,1)] after:shadow-[inset_0_0_0_1px_rgba(105,57,193,0.4)]"></span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="ρd__all ρd__div ρxeXe">
+            <div className="ρd__all ρd__div ρdkia6">
+              <div className="relative ρi ρhsMsa">
+                <video preload="auto" playsInline className="absolute object-cover w-full h-full">
+                  <source
+                    src="https://pub-9ba39fa3c4764cd0bb3a9579562941d6.r2.dev/GenAI%2F720p%2FGenAI-720p-mp4.mp4"
+                    type="video/mp4;codecs=hvc1"
+                  />
+                  <source
+                    src="https://pub-9ba39fa3c4764cd0bb3a9579562941d6.r2.dev/GenAI%2F720p%2FGenAI-720p-webm.webm"
+                    type="video/webm"
+                  />
+                </video>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProductsHeroSection;
