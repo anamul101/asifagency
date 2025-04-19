228cb535999fc714a0a8c211db2e8c6dfdad17d1
anamul101 <rjanamul01871@gmail.com>
2025-04-19 22:42:42 +0600

    product futuer

--- src/app/Products/page.tsx
+++ src/app/Products/page.tsx
@@ -1,4 +1,5 @@
 import FutureSection from '@/components/FutureSection'
+import ProductFeatures from '@/components/Products/ProductFeatures'
 import ProductsHeroSection from '@/components/Products/ProductsHeroSection'
 import VedioSection from '@/components/Products/VedioSection'
 import React from 'react'
@@ -7,6 +8,7 @@ export default function Products() {
   return (
     <>
       <ProductsHeroSection/>
+      <ProductFeatures/>
       <VedioSection/>
       <FutureSection/>
     </>
