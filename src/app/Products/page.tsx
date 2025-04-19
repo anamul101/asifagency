ff4b83a7108ce2fc95c6ca9fdbcc18f9da463ff6
anamul101 <rjanamul01871@gmail.com>
2025-04-19 18:14:20 +0600

    futuer section add

--- src/app/Products/page.tsx
+++ src/app/Products/page.tsx
@@ -1,8 +1,12 @@
+import FutureSection from '@/components/FutureSection'
 import ProductsHeroSection from '@/components/Products/ProductsHeroSection'
 import React from 'react'
 
 export default function Products() {
   return (
-    <ProductsHeroSection></ProductsHeroSection>
+    <>
+      <ProductsHeroSection/>
+      <FutureSection/>
+    </>
   )
 }
